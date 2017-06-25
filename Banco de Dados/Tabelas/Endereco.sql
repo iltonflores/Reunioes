7eb558ad2eb92adc6a37773d447ca99b09458971
iltonflores <ilton.flores@outlook.com>
2017-06-25 15:29:10 -0300

    atualizacao endereco
    Inclusão de campos que estavam faltando em endereco

--- Banco de Dados/Tabelas/Endereco.sql
+++ Banco de Dados/Tabelas/Endereco.sql
@@ -1,31 +1,35 @@
-/* Para impedir possíveis problemas de perda de dados, analise este script detalhadamente antes de executá-lo fora do contexto do designer de banco de dados.*/
-BEGIN TRANSACTION
-SET QUOTED_IDENTIFIER ON
-SET ARITHABORT ON
-SET NUMERIC_ROUNDABORT OFF
-SET CONCAT_NULL_YIELDS_NULL ON
+USE [Reunioes]
+GO
+
+/****** Object:  Table [dbo].[Endereco]    Script Date: 06/25/2017 15:16:21 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Endereco]') AND type in (N'U'))
+DROP TABLE [dbo].[Endereco]
+GO
+
+USE [Reunioes]
+GO
+
+/****** Object:  Table [dbo].[Endereco]    Script Date: 06/25/2017 15:16:21 ******/
 SET ANSI_NULLS ON
-SET ANSI_PADDING ON
-SET ANSI_WARNINGS ON
-COMMIT
-BEGIN TRANSACTION
 GO
-CREATE TABLE dbo.Endereco
-	(
-	id_endereco uniqueidentifier NOT NULL,
-	nm_rua nvarchar(510) NOT NULL,
-	nr_rua int NOT NULL,
-	nm_bairro nvarchar(510) NOT NULL,
-	nm_estado nvarchar(510) NOT NULL
-	)  ON [PRIMARY]
+
+SET QUOTED_IDENTIFIER ON
 GO
-ALTER TABLE dbo.Endereco ADD CONSTRAINT
-	PK_Endereco PRIMARY KEY CLUSTERED 
-	(
-	id_endereco
-	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+CREATE TABLE [dbo].[Endereco](
+	[id_endereco] [uniqueidentifier] NOT NULL,
+	[nm_rua] [nvarchar](510) NOT NULL,
+	[nr_rua] [int] NOT NULL,
+	[nm_bairro] [nvarchar](510) NOT NULL,
+	[nm_estado] [nvarchar](510) NOT NULL,
+	[nm_cidade] [nvarchar](510) NOT NULL,
+	[nr_cep] [bigint] NOT NULL,
+ CONSTRAINT [PK_Endereco] PRIMARY KEY CLUSTERED 
+(
+	[id_endereco] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
 GO
-ALTER TABLE dbo.Endereco SET (LOCK_ESCALATION = TABLE)
-GO
-COMMIT
+
+
