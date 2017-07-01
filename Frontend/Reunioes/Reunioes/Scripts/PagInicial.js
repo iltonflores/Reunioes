cdd9997582c4f8534aaef7c1b5c4c67ebbbbcda4
iltonflores <ilton.flores@outlook.com>
2017-07-01 16:37:18 -0300

    Testes cadastro
    Testes cadastro

--- Frontend/Reunioes/Reunioes/Scripts/PagInicial.js
+++ Frontend/Reunioes/Reunioes/Scripts/PagInicial.js
@@ -3,26 +3,12 @@
     var app = angular.module('PagInicial', ['ngRoute']);
 
     app.controller('HomeController', function ($scope) {
-        $scope.Mensagem = "Olá.  Esse é nosso primeiro contato com o AgularJS no ASP .NEt MVC.";
     });
 
-    app.controller('ReservasController', function ($scope, $http) {
-        $scope.Mensagem = "Teste Ilton Final";
+    app.controller('ReservasController', function ($scope) {
+    });
 
-        $scope.lista = [
-           {
-               name: 'Nexus S',
-               snippet: 'Fast just got faster with Nexus S.'
-           }, {
-               name: 'Motorola XOOM™ with Wi-Fi',
-               snippet: 'The Next, Next Generation tablet.'
-           }, {
-               name: 'MOTOROLA XOOM™',
-               snippet: 'The Next, Next Generation tablet.'
-           }
-        ];
-
-
+    app.controller('CadResController', function ($scope) {
     });
 
 })();
