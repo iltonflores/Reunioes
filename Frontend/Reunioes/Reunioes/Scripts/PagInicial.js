50a6ee6875b51bb6d692c6bffef34169b09c7b54
iltonflores <ilton.flores@outlook.com>
2017-07-02 14:25:47 -0300

    testes

--- Frontend/Reunioes/Reunioes/Scripts/PagInicial.js
+++ Frontend/Reunioes/Reunioes/Scripts/PagInicial.js
@@ -1,14 +1,13 @@
-﻿(function () {
-    
-    var app = angular.module('PagInicial', ['ngRoute']);
+﻿
+var app = angular.module('PagInicial', ['ngRoute']);
 
-    app.controller('HomeController', function ($scope) {
-    });
+app.controller('HomeController', function ($scope) {
+});
 
-    app.controller('ReservasController', function ($scope) {
-    });
+app.controller('ReservasController', ['$scope', function ($scope) {
+    $scope.function1 = function (msg) {
+        alert(msg + ' first function call!');
+    }
+}]);
 
-    app.controller('CadastraController', function ($scope) {
-    });
 
-})();
